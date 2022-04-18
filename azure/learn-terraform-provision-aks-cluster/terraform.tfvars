2b794ae038843bf6cfdd2854d998b35695ff9de6
BruceZhao98 <60870766+BruceZhao98@users.noreply.github.com>
2022-04-18 16:20:48 +0800

    Delete azure directory

--- azure/learn-terraform-provision-aks-cluster/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-appId    = "cbf23ea1-013e-4c11-a581-fcfa40a1f707"
-password = "AE4PRXCKTa8GsjbeOFOD-2h1fWiF.C6kvA"
--- azure/learn-terraform-provision-aks-cluster/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "appId" {
-  description = "cbf23ea1-013e-4c11-a581-fcfa40a1f707"
-}
-
-variable "password" {
-  description = "AE4PRXCKTa8GsjbeOFOD-2h1fWiF.C6kvA"
-}
