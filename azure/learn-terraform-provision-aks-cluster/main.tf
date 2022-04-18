5138d295726290aa54194124234b72241760b647
Brucezhao98 <brucezhao98@hotmail.com>
2022-04-18 08:28:26 +0000

    ZBC

---
+++ azure/learn-terraform-provision-aks-cluster/main.tf
@@ -0,0 +1,42 @@
+resource "random_pet" "prefix" {}
+
+provider "azurerm" {
+  features {}
+  subscription_id = 7d63d521-e065-4d22-9bf2-d4f581d99720
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${random_pet.prefix.id}-rg"
+  location = "East Asia"
+
+  tags = {
+    environment = "Demo"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "${random_pet.prefix.id}-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    environment = "Demo"
+  }
+}
---
+++ azure/learn-terraform-provision-aks-cluster/variables.tf
@@ -0,0 +1,11 @@
+variable "appId" {
+  description = "c8dc86d9-f32f-49de-a7ac-7ebddcc7ede7"
+}
+
+variable "password" {
+  description = "NyeiCm55X0Ww5ujPuBJ~-39fCLTVwFI0PW"
+}
+
+variable "subscription_id" {
+  description = "7d63d521-e065-4d22-9bf2-d4f581d99720"
+}
