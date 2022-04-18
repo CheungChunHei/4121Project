23138bac1fff9e9c0dd47a6d91dc8721c3925b64
BruceZhao98 <60870766+BruceZhao98@users.noreply.github.com>
2022-04-18 19:18:12 +0800

    Delete learn-terraform-provision-aks-cluster directory

--- learn-terraform-provision-aks-cluster/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-appId    = "c8dc86d9-f32f-49de-a7ac-7ebddcc7ede7"
-password = "6vBjbnluLoloGlPOfRMUu1vQV~V2lxflkm"
--- learn-terraform-provision-aks-cluster/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "appId" {
-  description = "c8dc86d9-f32f-49de-a7ac-7ebddcc7ede7"
-}
-
-variable "password" {
-  description = "6vBjbnluLoloGlPOfRMUu1vQV~V2lxflkm"
-}
